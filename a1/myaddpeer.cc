a335be37da9c5370e27ab8fc7518e7358a599f41
Jason Shao <jasonshao8@gmail.com>
2016-06-09 00:10:34 -0400

    Peer class

--- a1/myaddpeer.cc
+++ a1/myaddpeer.cc
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+struct Peer {
+    string ip;
+    int port;
+};
 
 int main(int argc, char* argv[]) {
     if (argc != 1 && argc != 3) {
         cerr << "Invalid Input" << endl;
         return -1;
+    }
+
+    vector<Peer> peers;
+    if (argc == 1) {
+
+    } else {
+
     }
     return 0;
 }
